4ed1cf0ec7b012ab075a93dc6b1b5180aed54284
Alexis Duchemann <alexisduchemann@icloud.com>
2024-04-06 11:46:35 +0200

    feat: add ssh connection information after vm creation

--- src/ui/dashboard/create-vm.tsx
+++ src/ui/dashboard/create-vm.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react"
 import { Button } from "../button"
 import Modal from "../modal"
 import { launchCreateResources } from "@/lib/vms/create-vm"
+import { ClipboardDocumentIcon } from "@heroicons/react/24/outline"
+import { lusitana } from "../fonts"
 
 interface CreateVMProps {
   machine: string
@@ -38,6 +40,13 @@ const CreateVM = ({
     }
   }
 
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => alert(`Copied to clipboard: ${text}`))
+      .catch((error) => setError(`Failed to copy: ${error}`))
+  }
+
   return (
     <>
       <Button
@@ -66,9 +75,29 @@ const CreateVM = ({
             </div>
           )}
           {fqdn && (
-            <div>
-              <p>SSH connection: notadmin@{fqdn}</p>
-              <p>Password: Pa$$w0rd92</p>
+            <div className="">
+              <div className={`${lusitana.className} text-2xl`}>
+                <p>✅ VM created successfully!</p>
+              </div>
+              <div className="mt-5 items-center">
+                <div className="flex">
+                  <div className="flex">
+                    <p>SSH connection:</p>
+                    <p className="text-bold">notadmin@{fqdn}</p>
+                  </div>
+                  <ClipboardDocumentIcon
+                    onClick={() => copyToClipboard("notadmin@{fqdn}")}
+                    className="w-6 h-6 cursor-pointer"
+                  />
+                </div>
+                <div className="flex">
+                  <p>Password: Pa$$w0rd92</p>
+                  <ClipboardDocumentIcon
+                    onClick={() => copyToClipboard("Pa$$w0rd92")}
+                    className="w-6 h-6 cursor-pointer"
+                  />
+                </div>
+              </div>
             </div>
           )}
           {error && <p>{error}</p>}
